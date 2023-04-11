d70fb28e81090714e98cccf0d488495ba68c14e3
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-11 11:00:55 +0900

    02:decttype(変数)

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 int main(int, char**) {
     std::cout << "Hello, world!\n";
 
-    // C++11 auto型推論
+    // 01:C++11 auto型推論
     auto word = "Hello, world2!\n";
     std::cout << word;
 
+    // 02:decttype(変数)
+    // あまり使い道がわからない・・・。
+    //  sizeofにもdecttypeには丸括弧が必須
+    auto i = 10;
+    decltype(i) j = i;
+    int m = sizeof (j);
+
+    i = 40;
+    std::cout << j << "," << i <<  "," << m << "byte\n";
 }
