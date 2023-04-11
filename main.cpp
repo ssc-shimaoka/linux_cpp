ce9653389a6d498b32764f048df39f3b7546095a
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-11 10:33:27 +0900

    auto型推論

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
-    std::cout << "Hello, world!\n";
-   std::cout << "Hello, world!\n";
+
+    // C++11 auto型推論
+    auto word = "Hello, world2!\n";
+    std::cout << word;
+
 }
