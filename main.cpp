a53030b14af2c8c6491a3e8af366731f895643a0
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-12 11:35:54 +0900

    クラステンプレート01

--- main.cpp
+++ main.cpp
@@ -1,53 +1,24 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <tuple> //タプル
 
-struct MyInt
+template <class T>
+class Stack
 {
-    int value;
-    /* data */
+private:
+    T* stack;
+    int index;
+public:
+    Stack(int size) : index(0) { stack = new T[size]; }
+    ~Stack() { delete [] stack; }
+    void push(T item) { stack[index++] = item; }
+    T pop() { return stack[--index]; }
 };
-
-//operator...[<]の演算子を独自のものに上書きしている。
-bool operator<(const MyInt& a, const MyInt& b) noexcept
-{
-    return a.value < b.value;
-}
-
-struct Person
-{
-    int id;
-    int age;
-    std::string name;
-    /* data */
-};
-
-bool operator<(const Person& a, const Person& b) noexcept
-{
-    return std::tie(a.id, a.age, a.name) < std::tie(b.id, b.age, b.name);
-}
-
-
-
 
 int main(int, char**) {
-    std::map<MyInt, int> m1{
-        {MyInt{1}, 3},
-        {MyInt{2}, 1},
-        {MyInt{3}, 4},
-    };
-    std::cout << m1[MyInt{2}]  << std::endl;
-    //追加
-    std::cout << m1[MyInt{3}]  << std::endl;
-
-    std::map<Person, int> m2{
-        {Person{1, 18, "Alice"}, 3},
-        {Person{2, 30, "Bob"}, 1},
-        {Person{3, 30, "carol"}, 5},
-    };
-    std::cout << m2[Person{2,30,"Bob"}]  << std::endl;
-    //追加
-    std::cout << m2[Person{1,18,"Alice"}]  << std::endl;
+    Stack<int> stack(256);
+    stack.push(123);
+    stack.push(456);
+    // 出力
+    std::cout << stack.pop() << std::endl;
+    std::cout << stack.pop() << std::endl;
+    std::cout << stack.pop() << std::endl;
 }
