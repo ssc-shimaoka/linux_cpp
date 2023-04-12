f92d477ff4e689b77d7a77a8d0e8d4c299aaf3d9
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-12 15:41:50 +0900

    ラムダ式01

--- main.cpp
+++ main.cpp
@@ -1,30 +1,19 @@
-// このソースをビルドすると、ビルドエラーの原因にassertのエラーメッセージが出る。
-// /home/shimaoka/source/main.cpp:7:24: error: static assertion failed: バッファサイズが0です
-
-#include <cstddef>
-
-template <std::size_t SIZE>
-
-class Buffer
-{
-    static_assert(SIZE > 0, "バッファサイズが0です");
-private:
-
-    /* data */
-public:
-    Buffer(/* args */);
-    ~Buffer();
-
-Buffer::Buffer(/* args */)
-{
-}
-
-Buffer::~Buffer()
-{
-}
-};
+#include <algorithm>
+#include <iostream>
+#include <vector>
 
 int main()
 {
-    Buffer<0> buf;
+    const int array[] = {1,2,3,4,5,6};
+    //vectorはシーケンスコンテナの一種で、各要素は線形に、順序を保ったまま格納される。
+    std::vector<int> v{array, array + 6};
+
+    std::cout << "コンテナのサイズは" << v.size() << std::endl;
+
+    //イテレータ範囲[first, last)から、条件を満たしている要素の数を数える。
+    //条件面にてラムダ式を使っている
+    auto c = std::count_if(v.begin(), v.end(), [](int elem) -> bool {return elem %2 == 0; });
+
+    //条件に合うのは「2・4・6」の3つ
+    std::cout << c << std::endl;
 }
