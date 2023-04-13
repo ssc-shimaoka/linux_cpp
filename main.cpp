b032cc614266de091fd9f1ff33516d4bc89c9001
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-13 16:40:21 +0900

    auto_ptrをunique_ptrに変更

--- main.cpp
+++ main.cpp
@@ -5,13 +5,13 @@
 
 int main()
 {
-    //Keyクラスのインスタンス作成(auto_ptr)
+    //Keyクラスのインスタンス作成(unique_ptr)
     //Room* room501 = new Room(501);
-    std::auto_ptr<Room> room501 (new Room(501));
+    std::unique_ptr<Room> room501 (new Room(501));
     std::cout << "作成した部屋番号は" << room501->getRoomNo() << std::endl;
 
     //Room* room502 = new Room(502);
-    std::auto_ptr<Room> room502 (new Room(502));
+    std::unique_ptr<Room> room502 (new Room(502));
     std::cout << "作成した部屋番号は" << room502->getRoomNo() << "\n" << std::endl;
 
     // 現在の状態取得1
