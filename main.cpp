6300b32092b0522dfc5227f6282a227218437607
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-14 09:38:33 +0900

    firstcommit

--- main.cpp
+++ main.cpp
@@ -3,26 +3,7 @@
 
 int main()
 {
-    // shared_ptr生成
-    std::shared_ptr<int> sharedPtr(new int(42));
 
-    // shared_ptrを使ってリソースにアクセス
-    int value = *sharedPtr;
-
-    value = 10;
-    std::cout << value << std::endl;
-
-    // リソースの所有権の移譲
-    std::shared_ptr<int> otherSharedPrt = sharedPtr;
-
-    // カスタムデリータの指定
-    std::shared_ptr<int> customDeleterSharedPrt(new int[10], [](int* p){
-        delete[] p;
-    });
-
-    // std::weak_ptrと連携
-    std::shared_ptr<int> sharedPtr1(new int(42));
-    std::weak_ptr<int> weakPtr(sharedPtr1);
 
     return 0;
 }
