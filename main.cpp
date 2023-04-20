0cfbf3ad01293a9ccda04d2c4daf0059d45c325e
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-20 16:51:54 +0900

    ディレクトリ操作

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include <memory>
 #include <iostream>
+#include <filesystem>
+
+using namespace std ;
 
 int main()
 {
+  // 現在の作業ディレクトリを取得
+  filesystem::path p = filesystem::current_path();
+  cout << p << endl;
 
-    return 0;
+  // 現在の作業ディレクトリを設定
+  filesystem::current_path("/tmp");
+  cout << filesystem::current_path() << endl;
 }
