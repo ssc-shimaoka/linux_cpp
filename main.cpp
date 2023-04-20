3e7816eb31425b054d42dc19147d5a141a4bfec6
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-20 16:38:38 +0900

    例外処理追加

--- main.cpp
+++ main.cpp
@@ -1,8 +1,29 @@
-#include <memory>
 #include <iostream>
+#include <exception>
+#include <stdexcept>
 
+using namespace std;
 int main()
 {
 
-    return 0;
+  try {
+    bool is_error = true;
+
+    // なんらかのエラーが発生した
+    if (is_error) { 
+      throw runtime_error("error!");
+    } else {
+        // 何もしない
+    }
+  }
+
+  catch (exception& e) {
+    // 例外を捕捉(// 実装定義のエラー内容を取得する)
+    cout << e.what() << endl;
+    return -1;
+  }
+
+  // 正常終了
+  cout << "正常終了" << endl;
+  return 0;
 }
