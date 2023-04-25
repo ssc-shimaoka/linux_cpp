f31f46a1b632cb69f64226bcf75c72c34d6cb1ea
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-25 14:04:51 +0900

    seekp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
-#include <memory>
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
+    const char *fileName = "/home/shimaoka/ピクチャ/data01";
 
-    return 0;
+    //ofstreamを読み取りモードで開き、末尾に移動
+    std::ofstream ofs(fileName, std::ios::in | std::ios::ate);
+    if (!ofs)
+    {
+        std::cout << "ファイルが開けませんでした。" << std::endl;
+        std::cin.get();
+        return 0;
+    }
+
+    //現在のファイル位置を表示
+    std::cout << "現在のファイル位置: " << ofs.tellp() << std::endl;
+
+    //ofs << "abcde" << std::endl;
+    std::string moji = "abcde";
+    for(auto i = 0; i < moji.length(); i++){
+        std::cout << "moji: " << moji[i] << std::endl;
+        ofs << moji[i];
+        std::cout << "現在のファイル位置: " << ofs.tellp() << std::endl;
+    }
+
+    
+
+    //現在の位置から2文字分戻る
+    ofs.seekp(-2, std::ios::cur);
+
+    std::cout << "現在のファイル位置: " << ofs.tellp() << std::endl;
+    ofs << "abcde" << std::endl;
+
+    std::cin.get();
 }
