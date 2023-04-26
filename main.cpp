2e871037102c3901c8e3afdee40b621ea69ff3dd
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-26 13:24:15 +0900

    実装

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
-#include <memory>
 #include <iostream>
+#include <future>
+#include <thread>
+#include <utility>
+
+void calc(std::promise<int> p)
+{
+  int sum = 0;
+  for (int i = 0; i <= 10; ++i) {
+    sum += i + 1;
+  }
+
+  p.set_value(sum); // 結果値を書き込む
+}
 
 int main()
 {
+  std::promise<int> p;
+  std::future<int> f = p.get_future();
 
-    return 0;
+  // 別スレッドで計算を行う
+  std::thread t(calc, std::move(p));
+
+  // calc()によって書き込まれた結果を取得
+  std::cout << f.get() << std::endl;
+
+  t.join();
 }
