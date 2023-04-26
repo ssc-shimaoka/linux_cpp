2962b1e9a2a48258ef674e5b6bc6f32cd5b27a6f
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-04-26 14:10:23 +0900

    実装

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <memory>
 #include <iostream>
+#include <ctime>
 
 int main()
 {
+	time_t curr_time;
+	curr_time = time(NULL);
 
-    return 0;
+	char *tm = ctime(&curr_time);
+	std::cout << "Today is : " << tm << std::endl;
+	
+	return 0;
 }
