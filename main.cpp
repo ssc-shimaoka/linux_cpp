917ad1c19b72af7c07dad2c6b4e618f33f655eaa
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2023-05-09 10:43:19 +0900

    実装

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-#include <memory>
 #include <iostream>
+#include <cstddef>
 
 int main()
 {
-
-    return 0;
+  std::ios str(NULL);
+  std::cout << "exceptions = " << str.exceptions() << '\n';
+  std::cout << "rdstate = " << str.rdstate() << '\n';
+  try {
+    str.exceptions(std::ios_base::failbit | std::ios_base::badbit);
+  } catch (std::ios_base::failure& e) {
+    std::cout << "exceptions = " << str.exceptions() << '\n';
+    std::cout << "rdstate = " << str.rdstate() << '\n';
+    std::cout << e.what() << '\n';
+  }
+  std::cout << "end\n";
 }
